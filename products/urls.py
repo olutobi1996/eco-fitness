24018fd84ab2e914c06d9cfd5bcb252d28d4af22
olutobi Adebayo <olutobi1996@icloud.com>
2025-02-09 22:17:15 +0000

    product_detail.html added

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from . import views  # Import views from the same directory
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('<int:pk>/', views.product_detail, name='product_detail'),
+    path('<product_id>', views.product_detail, name='product_detail'),
 ]
