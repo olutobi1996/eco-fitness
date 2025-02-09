bc0eeb5baa6cb74b4af15a0298ad7c845b772477
olutobi Adebayo <olutobi1996@icloud.com>
2025-02-09 22:02:40 +0000

    product.html added

--- products/views.py
+++ products/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+    
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'products/product_list.html', {'products': products})
