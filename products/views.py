77f3f63bf6ff9c90d3c36be310f4d81681614e66
olutobi Adebayo <olutobi1996@icloud.com>
2025-02-09 22:17:15 +0000

    product_detail.html added

--- products/views.py
+++ products/views.py
@@ -11,7 +11,19 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
-    
+
+    def product_detail(request, product_id):
+
+
+        product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
+
+
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'products/product_list.html', {'products': products})
