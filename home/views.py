b1e83911d6a311e8d158a9b7e048f1223a81114c
olutobi Adebayo <olutobi1996@icloud.com>
2025-02-10 20:25:59 +0000

    bag, profile, checkout apps

--- home/views.py
+++ home/views.py
@@ -6,3 +6,31 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def plans(request):
+    return render(request, 'plans.html')
+
+def reviews(request):
+    return render(request, 'reviews.html')
+
+def shop(request):
+    return render(request, 'shop.html')
+
+def community(request):
+    return render(request, 'community.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def product_list(request):
+    return render(request, 'products/product_list.html') 
+
+
+def add_product(request):
+    return render(request, 'products/add_product.html')
