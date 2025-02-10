48bccb8ba59655bd7fc92821b514b2ebad6c2c93
olutobi Adebayo <olutobi1996@icloud.com>
2025-02-10 20:25:59 +0000

    bag, profile, checkout apps

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('', views.product_list, name='products'), 
+     path('add/', views.add_product, name='add_product'), 
+    path('plans/', views.plans, name='plans'),  
+    path('reviews/', views.reviews, name='reviews'),  
+    path('shop/', views.shop, name='shop'),  
+    path('community/', views.community, name='community'),  
+    path('contact/', views.contact, name='contact'),  
+    path('profile/', views.profile, name='profile'),
+    path('cart/', views.cart, name='cart'),
 ]
