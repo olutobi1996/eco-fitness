ecb894b4e44b7f03cefe6c20537649dd41fb1eff
olutobi Adebayo <olutobi1996@icloud.com>
2025-02-10 20:44:02 +0000

    bag.html template added(others also

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+
+def view_bag(request):
+    return render(request, 'bag/bag.html')  # ✅ Ensure 'bag.html' exists
+
