2d757c175723f7cb0172e9697f5b3219c04a9cdb
olutobi Adebayo <olutobi1996@icloud.com>
2025-02-12 14:05:18 +0000

    bag views.py templated edited

--- bag/views.py
+++ bag/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.contrib import messages 
-
+from django.contrib import messages
+from .models import Product
 
 def add_to_bag(request, product_id):
-    from products.models import Product  
+    """ Add a quantity of the specified product to the shopping bag """
     product = get_object_or_404(Product, pk=product_id)
-
-
     try:
         quantity = int(request.POST.get('quantity', 1))
         if quantity < 1:
@@ -16,13 +14,21 @@ def add_to_bag(request, product_id):
         messages.error(request, "Invalid quantity entered.")
         return redirect(reverse('product_detail', args=[product_id]))
     
+    size = request.POST.get('product_size', None)
     redirect_url = request.POST.get('redirect_url', reverse('view_bag'))
+    
     bag = request.session.get('bag', {})
     
     if str(product_id) in bag:
-        bag[str(product_id)] += quantity
+        if size:
+            if size in bag[str(product_id)]['sizes']:
+                bag[str(product_id)]['sizes'][size] += quantity
+            else:
+                bag[str(product_id)]['sizes'][size] = quantity
+        else:
+            bag[str(product_id)]['quantity'] += quantity
     else:
-        bag[str(product_id)] = quantity
+        bag[str(product_id)] = {'quantity': quantity, 'sizes': {size: quantity} if size else {}}
     
     request.session['bag'] = bag
     messages.success(request, f'Added {quantity} x {product.name} to your bag')
@@ -47,5 +53,3 @@ def remove_from_bag(request, product_id):
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
         return redirect(reverse('view_bag'))
-
-
