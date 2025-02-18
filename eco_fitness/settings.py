acd35e3e5bca6007d355fe82b6e2967755242744
olutobi Adebayo <olutobi1996@icloud.com>
2025-02-18 16:52:01 +0000

    checkout.html, settings edit

--- eco_fitness/settings.py
+++ eco_fitness/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'eco_fitness.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -82,8 +86,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+             'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+                ]
         },
     },
 ]
--- eco_fitness/urls.py
+++ eco_fitness/urls.py
@@ -1,18 +1,10 @@
-"""eco_fitness URL Configuration
+"""
+eco_fitness URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -22,6 +14,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),  # Admin Panel
     path('accounts/', include('allauth.urls')),  # User Authentication
     path('', include('home.urls')),  # Home App
-    path('bag/', include('bag.urls')),
-    path('products/', include('products.urls')),  # Ensure Products App URLs are included
+    path('bag/', include('bag.urls')),  # Shopping Bag
+    path('products/', include('products.urls')),  # Product Pages
+    path('checkout/', include('checkout.urls')),  # Checkout App
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
