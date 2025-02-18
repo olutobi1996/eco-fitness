2a845cc61a3d8e693dd4f262f65b1a1b8ae206c7
olutobi Adebayo <olutobi1996@icloud.com>
2025-02-18 09:38:12 +0000

    checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,63 @@
+import uuid
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+from products.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    """ Model to store customer orders """
+    order_number = models.CharField(max_length=32, unique=True, editable=False)
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    phone_number = models.CharField(max_length=20)
+    country = models.CharField(max_length=50)
+    postcode = models.CharField(max_length=20, blank=True, null=True)
+    city = models.CharField(max_length=50)
+    address_line1 = models.CharField(max_length=100)
+    address_line2 = models.CharField(max_length=100, blank=True, null=True)
+    state_or_region = models.CharField(max_length=50, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def generate_order_number(self):
+        """ Generate a unique order number """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """ Recalculate order totals, including delivery costs """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """ Assign order number if not set """
+        if not self.order_number:
+            self.order_number = self.generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'Order {self.order_number}'
+
+
+class OrderLineItem(models.Model):
+    """ Model to store individual items within an order """
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product_size = models.CharField(max_length=5, blank=True, null=True)  # Example: XS, S, M, L, XL
+    quantity = models.PositiveIntegerField(default=1)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, editable=False)
+
+    def save(self, *args, **kwargs):
+        """ Calculate line item total before saving """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.quantity} x {self.product.name} in Order {self.order.order_number}'
