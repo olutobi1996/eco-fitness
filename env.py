46a81ce9a808328b3d83422ec3da96b0d4442218
olutobi Adebayo <olutobi1996@icloud.com>
2025-02-20 13:20:50 +0000

    JS stripe form handle submission edit

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+os.environ.setdefault('MY_ENVIRONMENT_VARIABLE', 'variable_value')
+
+os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51QuBP5F3sMCxmWUJ2FV3nIHuBXEWBHl1L27Q7ZAmVxvckYNjNQfv6c5NidU3sUMgMqW7Dm78nVUHVM8ryX3bLAdN00etfHfRSs"
+os.environ["STRIPE_SECRET_KEY"] = "sk_test_51QuBP5F3sMCxmWUJyVJCyLfcRMAuiexloOR62tk0Ye8Gvm8KTUqJLSOxvTBa2v0VYYXAYKmNWgkbGBOCe2mO9Vet00XbgxUXR7"
+os.environ["STRIPE_WH_SECRET"] = "your_webhook_secret_here"
