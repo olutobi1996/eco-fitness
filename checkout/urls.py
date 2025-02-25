5f4180762e805b436d9212425c24e6729a77cb07
olutobi Adebayo <olutobi1996@icloud.com>
2025-02-25 12:57:29 +0000

    chache handling and payment intent for js and webhandlers.py

--- checkout/urls.py
+++ checkout/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_success/<order_number>/', views.checkout_success, name='checkout_success'),
     path('webhook/', webhooks.webhook, name='stripe_webhook'),
+    path('cache_checkout_data/', cache_checkout_data, name='cache_checkout_data'),
 ]
