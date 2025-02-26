17e3d8a2a8bfcbe7cd0483116808027c19ea4349
olutobi Adebayo <olutobi1996@icloud.com>
2025-02-26 21:39:27 +0000

    changed product model and added fixtures instead

--- eco_fitness/urls.py
+++ eco_fitness/urls.py
@@ -10,12 +10,16 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),  # Admin Panel
     path('accounts/', include('allauth.urls')),  # User Authentication
     path('', include('home.urls')),  # Home App
     path('bag/', include('bag.urls')),  # Shopping Bag
-    path('products/', include('products.urls')),  # Product Pages
+    path('products/', include('products.urls', namespace="products")),
     path('checkout/', include('checkout.urls')),  # Checkout App
 ] 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0]) 
 
--- home/views.py
+++ home/views.py
@@ -3,9 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    """ A view to return the index page """
+    return render(request, "home/index.html")  
 
-    return render(request, 'home/index.html')
 
 def plans(request):
     return render(request, 'plans.html')
