fb3ce4f8a444413cc4f4827f01e2329928428536
olutobi Adebayo <olutobi1996@icloud.com>
2025-02-27 20:15:34 +0000

    updated login form

--- checkout/admin.py
+++ checkout/admin.py
@@ -14,23 +14,24 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
     readonly_fields = ('order_number', 'created_at',
-                       'delivery_cost', 'order_total', 'grand_total','original_bag', 
-                       'stripe_pid')
+                       'delivery_cost', 'order_total', 'grand_total',
+                       'original_bag', 'stripe_pid')
 
     fields = ('order_number', 'created_at', 'full_name',
-              'email', 'phone_number', 'country', 'postcode', 
-              'city', 'address_line1', 'address_line2', 
-              'state_or_region', 'delivery_cost', 
-              'order_total', 'grand_total','original_bag', 
-                       'stripe_pid')
+              'email', 'phone_number', 'country', 'postcode',
+              'street_address1', 'street_address2', 'town_or_city',
+              'county', 'delivery_cost', 'order_total',
+              'grand_total', 'original_bag', 'stripe_pid')
 
     list_display = ('order_number', 'created_at', 'full_name',
                     'order_total', 'delivery_cost', 'grand_total')
 
     list_filter = ('created_at', 'grand_total', 'delivery_cost')
     search_fields = ('order_number', 'full_name', 'email')
-    
+
     ordering = ('-created_at',)  
 
 
 admin.site.register(Order, OrderAdmin)
+
+
