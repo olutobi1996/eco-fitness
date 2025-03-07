fd266132d75339892614d5ed29672df35152cc09
olutobi Adebayo <olutobi1996@icloud.com>
2025-03-07 16:04:30 +0000

    heroku

--- eco_fitness/settings.py
+++ eco_fitness/settings.py
@@ -147,11 +147,12 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Static & Media Files
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Use AWS for Static & Media Files
