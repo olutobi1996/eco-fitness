596f82b1ca68df7ada1bfa122a62bc3d17fe6339
olutobi Adebayo <olutobi1996@icloud.com>
2025-03-24 21:15:54 +0000

    shop/bundle model created

--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from products.views import shop_view 
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('add/', views.add_product, name='add_product'), 
     path('plans/', views.plans, name='plans'),  
-    path('shop/', views.shop, name='shop'),  
+     path('shop/', shop_view, name='shop'),  
     path('community/', views.community, name='community'),  
     path('contact/', views.contact, name='contact'),  
     path('profile/', views.profile, name='profile'),
