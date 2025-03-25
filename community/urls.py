c522c861ce9ce2f59b5a05352d484277cba894c0
olutobi Adebayo <olutobi1996@icloud.com>
2025-03-25 10:55:21 +0000

    crud functionality for user

--- community/urls.py
+++ community/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import community_home, create_post, post_detail
+from . import views
 
 urlpatterns = [
-    path("", community_home, name="community"),  
-    path("create-post/", create_post, name="create_post"),  
-    path("post/<int:post_id>/", post_detail, name="post_detail"),  
+    path('', views.community_home, name='community'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('create/', views.create_post, name='create_post'),
+    path('post/<int:post_id>/edit/', views.edit_post, name='edit_post'),
+    path('post/<int:post_id>/delete/', views.delete_post, name='delete_post'),
 ]
-
