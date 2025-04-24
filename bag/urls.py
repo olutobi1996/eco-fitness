3892a4c131e98ad4b56488f29376b99d7038aac0
olutobi Adebayo <olutobi1996@icloud.com>
2025-04-24 14:55:03 +0000

    update bag urls and template

--- bag/urls.py
+++ bag/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('bag/', views.view_bag, name='view_bag'),
-    path('add/<int:product_id>/', views.add_to_bag, name='add_to_bag'),
-    path('adjust/<int:product_id>/', views.adjust_bag, name='adjust_bag'),  
-    path('remove/<int:product_id>/', views.remove_from_bag, name='remove_from_bag'),  
+    path('', views.view_bag, name='view_bag'),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+    path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
+    path('remove/<item_id>/', views.remove_from_bag, name='remove_from_bag'),
 ]
 
