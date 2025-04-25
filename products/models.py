74f2724a54ca9ed406a477b02d93fb548a97c136
olutobi Adebayo <olutobi1996@icloud.com>
2025-04-25 15:08:06 +0000

    image fix

--- products/models.py
+++ products/models.py
@@ -13,7 +13,8 @@ class Category(models.Model):
     ]
     name = models.CharField(max_length=50, choices=CATEGORY_CHOICES, unique=True)
     friendly_name = models.CharField(max_length=100, null=True, blank=True)
-    image = models.ImageField(upload_to='categories/', null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
 
     def __str__(self):
         return self.friendly_name or self.name
@@ -30,7 +31,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_featured = models.BooleanField(default=False)  
@@ -53,7 +54,7 @@ class Review(models.Model):
 class Bundle(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    image = models.ImageField(upload_to="bundles/")
+    image = models.ImageField(null=True, blank=True)
     original_price = models.DecimalField(max_digits=6, decimal_places=2)
     discounted_price = models.DecimalField(max_digits=6, decimal_places=2)
 
