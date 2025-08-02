ed29e860b56342b82fb64a5d21162f0ae06691df
olutobi Adebayo <olutobi1996@icloud.com>
2025-08-02 20:52:28 +0000

    community blog post Tweaks and correct image feild for images to work proproperly

--- community/models.py
+++ community/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class BlogPost(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
