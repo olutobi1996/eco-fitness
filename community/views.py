9066e282378d49f427c07205b737477266d9ef36
olutobi Adebayo <olutobi1996@icloud.com>
2025-08-03 17:05:52 +0000

    custom storage fixes

--- community/views.py
+++ community/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import BlogPostForm
 from .models import BlogPost
+import logging
 
 
 def community_home(request):
@@ -10,18 +11,34 @@ def community_home(request):
     return render(request, "community/community.html", {"posts": posts})
 
 
+
+logger = logging.getLogger(__name__)
+
 @login_required
 def create_post(request):
     if request.method == "POST":
+        logger.info("Received POST request for blog post creation")
+        logger.info("FILES received: %s", request.FILES)
+
         form = BlogPostForm(request.POST, request.FILES)
-        if form.is_valid():
+        if form.is_valid():  
             post = form.save(commit=False)
             post.author = request.user
             post.save()
+
+            logger.info("IMAGE FIELD: %s", post.image)
+            logger.info("IMAGE NAME: %s", post.image.name)
+            logger.info("IMAGE URL: %s", post.image.url)
+
             return redirect("community")
+        else:
+            logger.warning("Form is invalid: %s", form.errors)
     else:
         form = BlogPostForm()
+
     return render(request, "community/create_post.html", {"form": form})
+
+
 
 
 def post_detail(request, post_id):
