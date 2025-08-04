fcee1d55ff7a291ca7df834b2fa43f2dacb25742
olutobi Adebayo <olutobi1996@icloud.com>
2025-08-04 18:18:34 +0000

    Add cancel route to subscriptions app

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import create_checkout_session, plans_view, subscription_success
+from .views import create_checkout_session, plans_view, subscription_success,  subscription_cancel
 from .webhooks import webhook
 
 urlpatterns = [
@@ -12,4 +12,5 @@ urlpatterns = [
     path("webhook/", webhook, name="stripe_webhook"),  # Use 'webhook' here
     path("plans/", plans_view, name="plans"),
     path("success/", subscription_success, name="subscription_success"),
+    path("cancel/", subscription_cancel, name="subscription_cancel")
 ]
